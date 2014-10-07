e198d779de38ccf60c3a00ed531d0ff30c88de1b
Cullen Jett <cullenjett@gmail.com>
2014-10-07 16:03:45 -0600

    Repo object updates on 'git push'

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,21 +1,11 @@
 class HooksController < ApplicationController
   def git_push_callback
+    @repo = Repo.find_by(name: params[:repository][:name])
     response = JSON.parse(request.body.read)
-    @repo = Repo.find_by(name: response['repository']['name'])
-
-    client = Octokit::Client.new(login: 'cullenjett', password: 'condor88')
-    repo = client.repository("cullenjett/#{@repo.name}")
-    description = repo.description
-    commits = repo.rels[:commits].get.data
-    date = commits.first.commit.author.date
-    message = commits.first.commit.message
-
-    @repo.update_attribute(description: description)
-    @repo.update_attribute(commit_message: message)
-    @repo.update_attribute(commit_date: date)
-
-    binding.pry
-
+    description = response['repository']['description']
+    message = response['head_commit']['message']
+    date = response['head_commit']['timestamp']
+    @repo.update(description: description, commit_message: message, commit_date: date)
     render nothing: true
   end
 end
