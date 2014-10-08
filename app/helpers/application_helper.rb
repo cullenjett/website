a73024f40fd99c5e8e12aba42bf43d45e8ee6eb3
Cullen Jett <cullenjett@gmail.com>
2014-10-07 21:25:37 -0600

    Minor UI adjustments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def print_date(date)
+    if date == Date.today
+      content_tag :span, 'Today', class: 'today-badge'
+    else
+      date.strftime('%a, %b %d %Y')
+    end
+  end
 end
