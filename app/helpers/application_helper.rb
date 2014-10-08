612bd024bc8841f227052e22cb8a7e07985cb709
Cullen Jett <cullenjett@gmail.com>
2014-10-07 23:12:18 -0600

    Style the Repo boxes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,10 @@ module ApplicationHelper
   def print_date(date)
     if date == Date.today
       content_tag :span, 'Today', class: 'today-badge'
+    elsif date > 1.week.ago
+      date.strftime('%A')
     else
-      date.strftime('%a, %b %d %Y')
+      date.strftime('%a, %b %d')
     end
   end
 end
